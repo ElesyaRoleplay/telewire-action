3e48b0f9d40f6e32a3b84470788761a06e75ce88
giopera <perigio735@gmail.com>
2024-02-17 23:45:43 +0100

    Godo

--- dist/index.js
+++ dist/index.js
@@ -52659,7 +52659,8 @@ Tag: ${process.env.GITHUB_REF}
   }
 };
 const output = evresp(ghevent);
-psimage = psimage.replace(".", "\\.");
+psimage = psimage.replace(".", "\.");
+console.log(psimage);
 bot.sendMessage(chatid, output, {
   parse_mode: "Markdownv2",
   message_thread_id: 56,
